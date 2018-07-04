b101e0344e761ffcb81c7007a3edaade7c697775
Samuel Razali <samuelrazali@gmail.com>
2018-07-04 15:18:36 +1000

    bull update

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // guys just for practice write some code or line and ask a pull request
 
 //Hi 1st update yo
+
+
+// 2nd update
+
+    //  /                       \
+    // /X/                       \X\
+    // |XX\         _____         /XX|
+    // |XXX\     _/       \_     /XXX|___________
+    // \XXXXXXX             XXXXXXX/            \\\
+    //   \XXXX    /     \    XXXXX/                \\\
+    //        |   0     0   |                         \
+    //         |           |                           \
+    //          \         /                            |______//
+    //           \       /                             |
+    //            | O_O | \                            |
+    //             \ _ /   \________________           |
+    //                        | |  | |      \         /
+    //  No Bullshit,          / |  / |       \______/
+    //   Please...            \ |  \ |        \ |  \ |
+    //                      __| |__| |      __| |__| |
+    //                      |___||___|      |___||___|
