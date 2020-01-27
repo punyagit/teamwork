c7caecf0d3967004de4c01e3ae087d090f8cd729
punya <punyachapagain@gmail.com>
2020-01-27 11:39:58 +1100

    add comment to test file

--- sum.js
+++ sum.js
@@ -1,19 +1,12 @@
-
 // If you run npm test you will see some test is failed
 // so request team to implement it
-function add(a,b){
-    // check and b is number or not h
-        return a + b
-    
+function add(a, b) {
+  // check a and b is number or not aswell
 }
 
-function sumAll(arr){
-// Array should only contain two number [1,4]
-// this function will count between two number "1+2+3+4 = 10"
+function sumAll(arr) {
+  // Array should only contain two number [1,4]
+  // this function will count between two number 1 to 4  "1+2+3+4 = 10"
 }
 
-
-
-module.exports = {add,sumAll};
-
-
+module.exports = { add, sumAll };
